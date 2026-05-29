b35e3ee95e387d55491a20d9c6f11892d6d4a701
isrotiya-islama <isrotiyaislama@hotmail.com>
2026-05-29 14:38:34 +0300

    New desening

--- Components/js/index.js
+++ Components/js/index.js
@@ -1,28 +1,37 @@
 //TODO: Button Up
 $('.back-to-top').click(function () {
-  $('body,html').animate({ scrollTop: 0}, 800); // 800 - Скорость анимации
-});
+	$('body,html').animate({ scrollTop: 0 }, 800) // 800 - Скорость анимации
+})
 
-$(window).scroll(function() { // Отслеживаем начало прокрутки
-  let scrolled = $(window).scrollTop(); // Вычисляем сколько было прокручено.
+$(window).scroll(function () {
+	// Отслеживаем начало прокрутки
+	let scrolled = $(window).scrollTop() // Вычисляем сколько было прокручено.
 
-  if(scrolled > 3) { // Если высота прокрутки больше 3 - показываем кнопку
-      $('.back-to-top').addClass('active');
-  } else {
-      $('.back-to-top').removeClass('active');
-  }
-});
-
-
+	if (scrolled > 3) {
+		// Если высота прокрутки больше 3 - показываем кнопку
+		$('.back-to-top').addClass('active')
+	} else {
+		$('.back-to-top').removeClass('active')
+	}
+})
 
 //TODO: Dowloader
 function triggerDownload(fileName) {
-    var element = document.createElement('a');
-    element.setAttribute('href', fileName);
-    element.setAttribute('download', fileName);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    // Происходит клик, словно совершил его сам программирующий ниндзя
-    element.click();
-    document.body.removeChild(element);
-  }
+	var element = document.createElement('a')
+	element.setAttribute('href', fileName)
+	element.setAttribute('download', fileName)
+	element.style.display = 'none'
+	document.body.appendChild(element)
+	// Происходит клик, словно совершил его сам программирующий ниндзя
+	element.click()
+	document.body.removeChild(element)
+}
+
+document.querySelectorAll('.dropdown > a').forEach(link => {
+	link.addEventListener('click', e => {
+		if (window.innerWidth <= 768) {
+			e.preventDefault()
+			link.parentElement.classList.toggle('open')
+		}
+	})
+})
